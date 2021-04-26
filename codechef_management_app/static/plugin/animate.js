b77622622657f412066b28461f5fe8c4161de0af
Rajni Gupta <rajnigupta2901@gmail.com>
2021-04-26 15:24:52 +0530

    first commit

---
+++ codechef_management_app/static/plugin/animate.js
@@ -0,0 +1,48 @@
+// for login form
+$(document).ready(function() {
+    $('.login-info-box').fadeOut();
+    $('.login-show').addClass('show-log-panel');
+});
+
+
+$('.login-reg-panel input[type="radio"]').on('change', function() {
+    if  ($('#log-login-show').is(':checked')) {
+        $('.register-info-box').fadeOut(); 
+        $('.login-info-box').fadeIn();
+        
+        $('.white-panel').addClass('right-log');
+        $('.register-show').addClass('show-log-panel');
+        $('.login-show').removeClass('show-log-panel');
+        
+    }
+    else if ($('#log-reg-show').is(':checked')) {
+        $('.register-info-box').fadeIn();
+        $('.login-info-box').fadeOut();
+        
+        $('.white-panel').removeClass('right-log');
+        
+        $('.login-show').addClass('show-log-panel');
+        $('.register-show').removeClass('show-log-panel');
+    }
+});
+
+// for tgline
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0, 1],
+    easing: "easeInOutQuad",
+    duration: 1250,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0.7,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
